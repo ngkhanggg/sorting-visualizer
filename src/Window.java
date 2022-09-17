0fb247f15e1aeb680b08d94c2a0fac93b13bea22
ngkhanggg <nguyenkhangchu2004@gmail.com>
2022-09-17 13:02:49 -0400

    added documentation and lambda

--- src/Window.java
+++ src/Window.java
@@ -3,25 +3,30 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Window extends JFrame implements ActionListener {
+    // frame info
     public static final int WIDTH = 1000;
     public static final int HEIGHT = 700;
 
+    // components
     private JButton shuffleButton, exitButton, sortButton[];
-    private JPanel optionsPanel;
+    private JPanel buttonsPanel;
 
+    // visualizer panel
     private Visualizer visualizer;
 
+    // names of sorting algorithms
     private final String sortersName[] = {"Bubble Sort", "Insertion Sort", "Merge Sort", "Selection Sort", "Quick Sort"};
 
     public Window() {
         visualizer = new Visualizer();
-        visualizer.repaint();
 
-        optionsPanel = new JPanel();
-        optionsPanel.setLayout(new FlowLayout());
-        optionsPanel.setPreferredSize(new Dimension(0, 40));
-        optionsPanel.setBackground(Color.BLACK);
+        // panel for different buttons
+        buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new FlowLayout());
+        buttonsPanel.setPreferredSize(new Dimension(0, 40));
+        buttonsPanel.setBackground(Color.BLACK);
 
+        // buttons for chosing between sorting algorithms
         sortButton = new JButton[5];
         for (int i = 0; i < sortButton.length; i++) {
             sortButton[i] = new JButton();
@@ -32,29 +37,31 @@ public class Window extends JFrame implements ActionListener {
             sortButton[i].setBackground(Color.BLACK);
             sortButton[i].setForeground(Color.WHITE);
             sortButton[i].addActionListener(this);
-            optionsPanel.add(sortButton[i]);
-            optionsPanel.add(Box.createHorizontalStrut(3));
+            buttonsPanel.add(sortButton[i]);
+            buttonsPanel.add(Box.createHorizontalStrut(3));
         }
 
+        // shuffle button
         shuffleButton = new JButton();
         shuffleButton.setFont(new Font("Cambria", Font.PLAIN, 20));
         shuffleButton.setText("SHUFFLE");
         shuffleButton.setForeground(Color.WHITE);
         shuffleButton.setBackground(Color.BLACK);
         shuffleButton.setFocusable(false);
-        shuffleButton.addActionListener(this);
+        shuffleButton.addActionListener((e) -> visualizer.generateArr(visualizer.getNumBars()));
 
+        // exit button
         exitButton = new JButton();
         exitButton.setFont(new Font("Cambria", Font.PLAIN, 20));
         exitButton.setText("EXIT");
         exitButton.setForeground(Color.WHITE);
         exitButton.setBackground(Color.BLACK);
         exitButton.setFocusable(false);
-        exitButton.addActionListener(this);
+        exitButton.addActionListener((e) -> System.exit(0));
 
-        optionsPanel.add(shuffleButton);
-        optionsPanel.add(Box.createHorizontalStrut(3));
-        optionsPanel.add(exitButton);
+        buttonsPanel.add(shuffleButton);
+        buttonsPanel.add(Box.createHorizontalStrut(3));
+        buttonsPanel.add(exitButton);
         
         this.setTitle("Sorting Visualizer");
         this.setSize(WIDTH, HEIGHT);
@@ -62,7 +69,7 @@ public class Window extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setLayout(new BorderLayout());
-        this.add(optionsPanel, BorderLayout.NORTH);
+        this.add(buttonsPanel, BorderLayout.NORTH);
         this.add(visualizer, BorderLayout.CENTER);
         this.setVisible(true);
     }
@@ -70,14 +77,6 @@ public class Window extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         Object a = e.getSource();
-        if (a == shuffleButton) {
-            visualizer.generateArr(visualizer.getNumBars());
-            visualizer.repaint();
-        }
-
-        if (a == exitButton) {
-            System.exit(0);
-        }
 
         for (int i = 0; i < sortButton.length; i++) {
             if (a == sortButton[i]) {
