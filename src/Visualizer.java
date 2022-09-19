95be4c8b3805107039eddd56f371b1d0ce668fbf
ngkhanggg <nguyenkhangchu2004@gmail.com>
2022-09-19 18:58:46 -0400

    added selection sort

--- src/Visualizer.java
+++ src/Visualizer.java
@@ -10,7 +10,7 @@ public class Visualizer extends JPanel {
     private final int WIDTH = 1000;
     private final int HEIGHT = 600;
 
-    private final int BAR_WIDTH = 10;
+    private final int BAR_WIDTH = 25;
     private final int NUM_BARS = WIDTH / BAR_WIDTH;
 
     // constructor
@@ -24,11 +24,11 @@ public class Visualizer extends JPanel {
     // paint
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        drawInitialBars(g);
+        draw(g);
     }
 
     // draw
-    public void drawInitialBars(Graphics g) {
+    public void draw(Graphics g) {
         for (int i = 0; i < NUM_BARS; i++) {
             int height = arr[i]*10;
             int x = i + (BAR_WIDTH - 1) * i;
@@ -57,18 +57,30 @@ public class Visualizer extends JPanel {
     // sort animation
     public void sortAnimate(String name) {
         BubbleSort bubbleSort = new BubbleSort(arr);
-
+        SelectionSort selectionSort = new SelectionSort(arr);
         Timer timer = new Timer(0, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (bubbleSort.isSorted()) {
-                    ((Timer) e.getSource()).stop();
-                    System.out.println(bubbleSort);
+                switch(name) {
+                    case "Bubble Sort":
+                        if (bubbleSort.isSorted()) {
+                            ((Timer)e.getSource()).stop();
+                        }
+                        else {
+                            bubbleSort.sort();
+                        }
+                        repaint();
+                        break;
+                    case "Selection Sort":
+                        if (selectionSort.isSorted()) {
+                            ((Timer)e.getSource()).stop();
+                        }
+                        else {
+                            selectionSort.sort();
+                        }
+                        repaint();
+                        break;
                 }
-                else {
-                    bubbleSort.sort();
-                }
-                repaint();
             }
         });
         timer.start();
