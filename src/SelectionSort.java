cf80a0b99a43353b957e419cff588c8a2be33b15
ngkhanggg <nguyenkhangchu2004@gmail.com>
2022-09-19 18:58:58 -0400

    selection sort

---
+++ src/SelectionSort.java
@@ -0,0 +1,26 @@
+public class SelectionSort extends Sorter {
+    private int arrayIndex = 0, compareIndex = arrayIndex + 1;
+
+    public SelectionSort(int[] arr) {
+        super(arr);
+    }
+
+    @Override
+    public String getName() {
+        return "Selection Sort";
+    }
+
+    public void sort() {
+        if (arr[compareIndex] < arr[arrayIndex]) {
+            swap(arrayIndex, compareIndex);
+        }
+
+        if (compareIndex+1 >= arr.length) {
+            arrayIndex++;
+            compareIndex = arrayIndex + 1;
+        }
+        else {
+            compareIndex++;
+        }
+    }
+}
